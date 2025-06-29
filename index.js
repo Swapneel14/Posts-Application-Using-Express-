2237e72b60cada384ff82dae719524286ed366d5
Swapneel14 <sarkarswapneel14@gmail.com>
2025-06-29 17:26:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const express= require("express");
+const app=new express();
+const port=3000;
+const path= require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+const {v4:uuidv4}=require('uuid');
+const mo=require("method-override");
+
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.urlencoded({extended:true}));
+app.use(mo("_method"));
+app.use(express.json());
+ let allposts=[
+    {user:"Virat Kohli",
+        post:"Just Finished Practice",
+        id:uuidv4()
+},
+{user:"Anupam Roy",
+        post:"What a Weather!!",
+        id:uuidv4()
+},
+{user:"Rahul",
+        post:"Just Made this site",
+        id:uuidv4()
+},
+{user:"Anonymus",
+        post:"Hello All",
+        id:uuidv4()
+},
+]
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{allposts});
+})
+app.get("/newpost",(req,res)=>{
+    res.render("create.ejs");
+})
+app.post("/",(req,res)=>{
+    let {user,post}=req.body;
+    let id=uuidv4();
+    allposts.push({user,post,id});
+    res.redirect("/");
+})
+app.delete("/post/:id",(req,res)=>{
+    let {id}=req.params;
+    let delpost=allposts.find((p)=>p.id===id);
+    allposts=allposts.filter(p=>p!=delpost);
+    res.redirect("/");
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log("Listenning to Port 3000")
+})
